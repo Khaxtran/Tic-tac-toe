c7baed2c7dfcd504e2b992c607bff86084b9edb9
Kha Tran <khatran961217@gmail.com>
2022-05-03 09:49:44 +0700

    Initial Commit

---
+++ Tic-tac-toe/Tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toeApp.swift
+//  Tic-tac-toe
+//
+//  Created by Kha Tran on 3/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
