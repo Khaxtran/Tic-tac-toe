7bc0875542e8b033c6feea1e244912844622670b
Kha Tran <khatran961217@gmail.com>
2022-05-03 10:56:02 +0700

    Initial commit
    Created UI board game view

--- Tic-tac-toe/ContentView.swift
+++ Tic-tac-toe/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.orange).opacity(0.7)
+                                .frame(width: geo.size.width/3 - 15,
+                                       height: geo.size.width/3 - 15)
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer,
+                                            boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
+        .padding()
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index})
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
